8d523236d56472b6acb2c595e4b3d64c31985e12
Gabriel Torres <elcomparosh97@gmail.com>
2024-09-15 19:37:34 -0600

    marin puto

--- Metodo PEPS/Form1.Designer.cs
+++ Metodo PEPS/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             btnRegistrarCompra = new Button();
             btnRegistrarVenta = new Button();
@@ -47,10 +48,11 @@
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
             label1.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = SystemColors.ActiveCaption;
-            label1.Location = new Point(254, 26);
+            label1.ForeColor = Color.CornflowerBlue;
+            label1.Location = new Point(186, 23);
             label1.Name = "label1";
             label1.Size = new Size(654, 29);
             label1.TabIndex = 0;
@@ -58,30 +60,43 @@
             // 
             // btnRegistrarCompra
             // 
-            btnRegistrarCompra.Location = new Point(579, 171);
+            btnRegistrarCompra.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnRegistrarCompra.BackColor = Color.White;
+            btnRegistrarCompra.FlatAppearance.BorderSize = 0;
+            btnRegistrarCompra.FlatAppearance.MouseOverBackColor = Color.CornflowerBlue;
+            btnRegistrarCompra.FlatStyle = FlatStyle.Flat;
+            btnRegistrarCompra.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
+            btnRegistrarCompra.Location = new Point(509, 169);
             btnRegistrarCompra.Name = "btnRegistrarCompra";
             btnRegistrarCompra.Size = new Size(100, 31);
             btnRegistrarCompra.TabIndex = 1;
             btnRegistrarCompra.Text = "Comprar";
-            btnRegistrarCompra.UseVisualStyleBackColor = true;
+            btnRegistrarCompra.UseVisualStyleBackColor = false;
             btnRegistrarCompra.Click += btnRegistrarCompra_Click_1;
             // 
             // btnRegistrarVenta
             // 
-            btnRegistrarVenta.Location = new Point(587, 310);
+            btnRegistrarVenta.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnRegistrarVenta.BackColor = Color.White;
+            btnRegistrarVenta.FlatAppearance.BorderSize = 0;
+            btnRegistrarVenta.FlatAppearance.MouseOverBackColor = Color.CornflowerBlue;
+            btnRegistrarVenta.FlatStyle = FlatStyle.Flat;
+            btnRegistrarVenta.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
+            btnRegistrarVenta.Location = new Point(507, 304);
             btnRegistrarVenta.Name = "btnRegistrarVenta";
             btnRegistrarVenta.Size = new Size(100, 31);
             btnRegistrarVenta.TabIndex = 2;
             btnRegistrarVenta.Text = "Vender";
-            btnRegistrarVenta.UseVisualStyleBackColor = true;
+            btnRegistrarVenta.UseVisualStyleBackColor = false;
             btnRegistrarVenta.Click += btnRegistrarVenta_Click_1;
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label2.AutoSize = true;
             label2.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
             label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(544, 90);
+            label2.Location = new Point(462, 98);
             label2.Name = "label2";
             label2.Size = new Size(90, 21);
             label2.TabIndex = 3;
@@ -89,10 +104,11 @@
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label3.AutoSize = true;
             label3.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
             label3.ForeColor = SystemColors.ButtonHighlight;
-            label3.Location = new Point(793, 90);
+            label3.Location = new Point(711, 98);
             label3.Name = "label3";
             label3.Size = new Size(142, 21);
             label3.TabIndex = 4;
@@ -100,74 +116,82 @@
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label4.AutoSize = true;
             label4.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
             label4.ForeColor = SystemColors.ButtonHighlight;
-            label4.Location = new Point(267, 90);
+            label4.Location = new Point(185, 98);
             label4.Name = "label4";
-            label4.Size = new Size(65, 21);
+            label4.Size = new Size(161, 21);
             label4.TabIndex = 5;
-            label4.Text = "Fecha:";
+            label4.Text = "Fecha de Compra:";
             // 
             // txtCantidadCompra
             // 
+            txtCantidadCompra.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtCantidadCompra.Font = new Font("Yu Gothic Medium", 12F);
-            txtCantidadCompra.Location = new Point(544, 113);
+            txtCantidadCompra.Location = new Point(462, 121);
             txtCantidadCompra.Name = "txtCantidadCompra";
             txtCantidadCompra.Size = new Size(195, 33);
             txtCantidadCompra.TabIndex = 6;
             // 
             // txtPrecioUnitario
             // 
+            txtPrecioUnitario.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtPrecioUnitario.Font = new Font("Yu Gothic Medium", 12F);
-            txtPrecioUnitario.Location = new Point(793, 113);
+            txtPrecioUnitario.Location = new Point(711, 121);
             txtPrecioUnitario.Name = "txtPrecioUnitario";
             txtPrecioUnitario.Size = new Size(144, 33);
             txtPrecioUnitario.TabIndex = 7;
             // 
             // dtpFechaCompra
             // 
+            dtpFechaCompra.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             dtpFechaCompra.Font = new Font("Yu Gothic Medium", 12F);
             dtpFechaCompra.Format = DateTimePickerFormat.Short;
-            dtpFechaCompra.Location = new Point(267, 113);
+            dtpFechaCompra.Location = new Point(185, 121);
             dtpFechaCompra.Name = "dtpFechaCompra";
             dtpFechaCompra.Size = new Size(220, 33);
             dtpFechaCompra.TabIndex = 8;
             // 
             // dtpFechaVenta
             // 
+            dtpFechaVenta.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             dtpFechaVenta.Font = new Font("Yu Gothic Medium", 12F);
             dtpFechaVenta.Format = DateTimePickerFormat.Short;
-            dtpFechaVenta.Location = new Point(397, 252);
+            dtpFechaVenta.Location = new Point(307, 255);
             dtpFechaVenta.Name = "dtpFechaVenta";
             dtpFechaVenta.Size = new Size(220, 33);
             dtpFechaVenta.TabIndex = 12;
             // 
             // txtCantidadVenta
             // 
+            txtCantidadVenta.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtCantidadVenta.Font = new Font("Yu Gothic Medium", 12F);
-            txtCantidadVenta.Location = new Point(674, 257);
+            txtCantidadVenta.Location = new Point(584, 255);
             txtCantidadVenta.Name = "txtCantidadVenta";
             txtCantidadVenta.Size = new Size(195, 33);
             txtCantidadVenta.TabIndex = 11;
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label5.AutoSize = true;
             label5.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
             label5.ForeColor = SystemColors.ButtonHighlight;
-            label5.Location = new Point(397, 228);
+            label5.Location = new Point(307, 231);
             label5.Name = "label5";
-            label5.Size = new Size(65, 21);
+            label5.Size = new Size(144, 21);
             label5.TabIndex = 10;
-            label5.Text = "Fecha:";
+            label5.Text = "Fecha de Venta:";
             // 
             // label6
             // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label6.AutoSize = true;
             label6.Font = new Font("Yu Gothic Medium", 12F, FontStyle.Bold);
             label6.ForeColor = SystemColors.ButtonHighlight;
-            label6.Location = new Point(674, 233);
+            label6.Location = new Point(584, 231);
             label6.Name = "label6";
             label6.Size = new Size(90, 21);
             label6.TabIndex = 9;
@@ -175,18 +199,22 @@
             // 
             // dgvSaldos
             // 
+            dgvSaldos.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvSaldos.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
             dgvSaldos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvSaldos.Location = new Point(25, 390);
+            dgvSaldos.Location = new Point(29, 365);
             dgvSaldos.Name = "dgvSaldos";
-            dgvSaldos.Size = new Size(1187, 255);
+            dgvSaldos.ReadOnly = true;
+            dgvSaldos.Size = new Size(1021, 266);
             dgvSaldos.TabIndex = 13;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
             BackColor = Color.FromArgb(26, 26, 26);
-            ClientSize = new Size(1235, 657);
+            ClientSize = new Size(1083, 657);
             Controls.Add(dgvSaldos);
             Controls.Add(dtpFechaVenta);
             Controls.Add(txtCantidadVenta);
@@ -202,6 +230,7 @@
             Controls.Add(btnRegistrarCompra);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "PEPS";
--- Metodo PEPS/Form1.cs
+++ Metodo PEPS/Form1.cs
@@ -8,6 +8,63 @@ namespace Metodo_PEPS
             InitializeComponent(); 
             ConfigurarDataGridView();
         }
+        //private void ConfigurarDataGridView()
+        //{
+        //    dgvSaldos.Columns.Add("Fecha", "Fecha");
+        //    dgvSaldos.Columns.Add("Concepto", "Concepto");
+        //    dgvSaldos.Columns.Add("CantidadEntrada", "Cantidad (Entrada)");
+        //    dgvSaldos.Columns.Add("ValorUnitarioEntrada", "Valor unitario (Entrada)");
+        //    dgvSaldos.Columns.Add("ValorTotalEntrada", "Valor total (Entrada)");
+        //    dgvSaldos.Columns.Add("CantidadSalida", "Cantidad (Salida)");
+        //    dgvSaldos.Columns.Add("ValorUnitarioSalida", "Valor unitario (Salida)");
+        //    dgvSaldos.Columns.Add("ValorTotalSalida", "Valor total (Salida)");
+        //    dgvSaldos.Columns.Add("CantidadSaldo", "Cantidad (Saldo)");
+        //    dgvSaldos.Columns.Add("ValorTotalSaldo", "Valor total (Saldo)");
+        //}
+
+        //private void MostrarSaldos()
+        //{
+        //    dgvSaldos.Rows.Clear();
+
+        //    var historial = inventario.ObtenerHistorial();
+
+        //    // Llenamos el DataGridView con el historial
+        //    foreach (var registro in historial)
+        //    {
+        //        dgvSaldos.Rows.Add(
+        //            registro.Fecha.ToString("dd-MMM"),
+        //            registro.Concepto,
+        //            registro.CantidadEntrada.HasValue ? registro.CantidadEntrada.Value.ToString() : "",
+        //            registro.ValorUnitarioEntrada.HasValue ? registro.ValorUnitarioEntrada.Value.ToString("C") : "",
+        //            registro.ValorTotalEntrada.HasValue ? registro.ValorTotalEntrada.Value.ToString("C") : "",
+        //            registro.CantidadSalida.HasValue ? registro.CantidadSalida.Value.ToString() : "",
+        //            registro.ValorUnitarioSalida.HasValue ? registro.ValorUnitarioSalida.Value.ToString("C") : "",
+        //            registro.ValorTotalSalida.HasValue ? registro.ValorTotalSalida.Value.ToString("C") : "",
+        //            registro.CantidadSaldo.ToString(), // Se muestra la cantidad de saldo restante en cada movimiento
+        //            registro.ValorTotalSaldo.ToString("C") // El valor total del saldo restante
+        //        );
+        //    }
+        //}
+
+        //private void btnRegistrarCompra_Click_1(object sender, EventArgs e)
+        //{
+        //    DateTime fecha = dtpFechaCompra.Value;
+        //    int cantidad = int.Parse(txtCantidadCompra.Text);
+        //    decimal precioUnitario = decimal.Parse(txtPrecioUnitario.Text);
+
+        //    inventario.RegistrarCompra(fecha, cantidad, precioUnitario);
+        //    MostrarSaldos();
+        //}
+
+        //private void btnRegistrarVenta_Click_1(object sender, EventArgs e)
+        //{
+        //    DateTime fecha = dtpFechaVenta.Value;
+        //    int cantidad = int.Parse(txtCantidadVenta.Text);
+
+        //    inventario.RegistrarVenta(fecha, cantidad);
+        //    MostrarSaldos();
+        //}
+
         private void ConfigurarDataGridView()
         {
             dgvSaldos.Columns.Add("Fecha", "Fecha");
@@ -24,65 +81,117 @@ namespace Metodo_PEPS
 
         private void MostrarSaldos()
         {
-            //dgvSaldos.Rows.Clear();
-
-            //var historial = inventario.ObtenerHistorial();
-
-            //// Llenamos el DataGridView con el historial, pero sin mostrar el valor unitario del saldo
-            //foreach (var registro in historial)
-            //{
-            //    dgvSaldos.Rows.Add(
-            //        registro.Fecha.ToString("dd-MMM"),
-            //        registro.Concepto,
-            //        registro.CantidadEntrada.HasValue ? registro.CantidadEntrada.Value.ToString() : "",
-            //        registro.ValorUnitarioEntrada.HasValue ? registro.ValorUnitarioEntrada.Value.ToString("C") : "",
-            //        registro.ValorTotalEntrada.HasValue ? registro.ValorTotalEntrada.Value.ToString("C") : "",
-            //        registro.CantidadSalida.HasValue ? registro.CantidadSalida.Value.ToString() : "",
-            //        registro.ValorUnitarioSalida.HasValue ? registro.ValorUnitarioSalida.Value.ToString("C") : "",
-            //        registro.ValorTotalSalida.HasValue ? registro.ValorTotalSalida.Value.ToString("C") : "",
-            //        registro.CantidadSaldo.ToString(), // Solo mostramos la cantidad del saldo
-            //        registro.ValorTotalSaldo.ToString("C") // El valor total del saldo se sigue mostrando
-            //    );
-            //}
-            dgvSaldos.Rows.Clear();
-
-            var historial = inventario.ObtenerHistorial();
-
-            // Llenamos el DataGridView con el historial
-            foreach (var registro in historial)
+            try
             {
-                dgvSaldos.Rows.Add(
-                    registro.Fecha.ToString("dd-MMM"),
-                    registro.Concepto,
-                    registro.CantidadEntrada.HasValue ? registro.CantidadEntrada.Value.ToString() : "",
-                    registro.ValorUnitarioEntrada.HasValue ? registro.ValorUnitarioEntrada.Value.ToString("C") : "",
-                    registro.ValorTotalEntrada.HasValue ? registro.ValorTotalEntrada.Value.ToString("C") : "",
-                    registro.CantidadSalida.HasValue ? registro.CantidadSalida.Value.ToString() : "",
-                    registro.ValorUnitarioSalida.HasValue ? registro.ValorUnitarioSalida.Value.ToString("C") : "",
-                    registro.ValorTotalSalida.HasValue ? registro.ValorTotalSalida.Value.ToString("C") : "",
-                    registro.CantidadSaldo.ToString(), // Se muestra la cantidad de saldo restante en cada movimiento
-                    registro.ValorTotalSaldo.ToString("C") // El valor total del saldo restante
-                );
+                dgvSaldos.Rows.Clear();
+                var historial = inventario.ObtenerHistorial();
+
+                foreach (var registro in historial)
+                {
+                    dgvSaldos.Rows.Add(
+                        registro.Fecha.ToString("dd-MMM"),
+                        registro.Concepto,
+                        registro.CantidadEntrada.HasValue ? registro.CantidadEntrada.Value.ToString() : "",
+                        registro.ValorUnitarioEntrada.HasValue ? registro.ValorUnitarioEntrada.Value.ToString("C") : "",
+                        registro.ValorTotalEntrada.HasValue ? registro.ValorTotalEntrada.Value.ToString("C") : "",
+                        registro.CantidadSalida.HasValue ? registro.CantidadSalida.Value.ToString() : "",
+                        registro.ValorUnitarioSalida.HasValue ? registro.ValorUnitarioSalida.Value.ToString("C") : "",
+                        registro.ValorTotalSalida.HasValue ? registro.ValorTotalSalida.Value.ToString("C") : "",
+                        registro.CantidadSaldo.ToString(),
+                        registro.ValorTotalSaldo.ToString("C")
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al mostrar los saldos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void btnRegistrarCompra_Click_1(object sender, EventArgs e)
         {
-            DateTime fecha = dtpFechaCompra.Value;
-            int cantidad = int.Parse(txtCantidadCompra.Text);
-            decimal precioUnitario = decimal.Parse(txtPrecioUnitario.Text);
+            try
+            {
+                if (ValidarDatosCompra())
+                {
+                    DateTime fecha = dtpFechaCompra.Value;
+                    int cantidad = int.Parse(txtCantidadCompra.Text);
+                    decimal precioUnitario = decimal.Parse(txtPrecioUnitario.Text);
 
-            inventario.RegistrarCompra(fecha, cantidad, precioUnitario);
-            MostrarSaldos();
+                    inventario.RegistrarCompra(fecha, cantidad, precioUnitario);
+                    MostrarSaldos();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al registrar la compra: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnRegistrarVenta_Click_1(object sender, EventArgs e)
         {
-            DateTime fecha = dtpFechaVenta.Value;
-            int cantidad = int.Parse(txtCantidadVenta.Text);
+            try
+            {
+                if (ValidarDatosVenta())
+                {
+                    DateTime fecha = dtpFechaVenta.Value;
+                    int cantidad = int.Parse(txtCantidadVenta.Text);
 
-            inventario.RegistrarVenta(fecha, cantidad);
-            MostrarSaldos();
+                    inventario.RegistrarVenta(fecha, cantidad);
+                    MostrarSaldos();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al registrar la venta: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidarDatosCompra()
+        {
+            try
+            {
+                // Validar que la cantidad sea un número entero positivo
+                if (!int.TryParse(txtCantidadCompra.Text, out int cantidad) || cantidad <= 0)
+                {
+                    MessageBox.Show("Por favor, ingrese una cantidad válida para la compra (número entero positivo).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+
+                // Validar que el precio unitario sea un número decimal positivo
+                if (!decimal.TryParse(txtPrecioUnitario.Text, out decimal precioUnitario) || precioUnitario <= 0)
+                {
+                    MessageBox.Show("Por favor, ingrese un precio unitario válido (número decimal positivo).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la validación de datos de compra: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private bool ValidarDatosVenta()
+        {
+            try
+            {
+                // Validar que la cantidad sea un número entero positivo
+                if (!int.TryParse(txtCantidadVenta.Text, out int cantidad) || cantidad <= 0)
+                {
+                    MessageBox.Show("Por favor, ingrese una cantidad válida para la venta (número entero positivo).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la validación de datos de venta: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
         }
     }
 }
